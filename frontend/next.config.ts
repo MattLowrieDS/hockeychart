d37b9af0de2b15839eadb70595c9c274184e35fb
MattLowrieDS <matt.lowrie.ds@gmail.com>
2026-03-25 20:23:18 -0600

    Minor change

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
       {
         source: '/api/:path*',
         // destination: 'http://localhost:8000/api/:path*',
-        destination: process.env.NEXT_PUBLIC_API_BASE_URL
+        destination: 'https://hockeychart-api.vercel.app/api/:path*'
       },
     ];
   },
